67341df3c3c4db738413bb328d40a33b2d6fa889
matheusfsantana <matheusfsantana99@gmail.com>
2024-12-02 16:47:24 -0300

    commit final

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Actor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name)  }
+  it { should validate_presence_of(:nacionality)  }
+  it { should validate_presence_of(:birth_date)  }
+  it { should have_many(:roles) }
 end
--- spec/system/user_add_role_to_actor_spec.rb
+++ spec/system/user_add_role_to_actor_spec.rb
@@ -17,10 +17,27 @@ describe 'Add role to actor ' do
     select 'Werewolf', from: 'Filme'
     click_on 'Registrar Papel'
 
-    expect(page).to have_css 'h1', text: 'Matheus'
-    expect(page).to have_content 'Brasileiro'
     expect(page).to have_css 'h2', text: 'Papéis'
     expect(page).to have_content 'Werewolf'
     expect(page).to have_content 'Lobisomem'
+    expect(page).to have_content 'Papel registrado com sucesso.'
+  end
+
+  it 'fail' do
+    user = FactoryBot.create(:user, email: 'joao@email.com', password: '123456789')
+    movie = FactoryBot.create(:movie, title: 'Werewolf', year: 1900)
+    actor = Actor.create!(name: "Matheus", birth_date: Date.today, nacionality: 'Brasileiro')
+    login_as user
+
+    visit root_path
+    within('nav') do
+      click_on 'Atores'
+    end
+    click_on 'Matheus'
+    click_on 'Registrar Papel'
+    fill_in 'Papel', with: ''
+    click_on 'Registrar Papel'
+
+    expect(page).to have_content 'Papel não foi registrado.'
   end
 end
